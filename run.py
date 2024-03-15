a71e6a6fa19446b10b13a3246029a67ea3d720ab
seanfreaney <seanfreaney123@gmail.com>
2024-03-15 11:28:42 +0000

    add welcome message

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ class Board:
     """
     def __init__(self, size):
         self.size = size
-        self.board = [["." for x in range(size)] for y in range(size)] #Project 3 logic/scope video
+        self.board = [["." for x in range(size)] for y in range(size)] #Project 3 scope video
     
     def print(self):
         for row in self.board:
@@ -28,6 +28,12 @@ def game():
     computer_board = Board(size)
 
     player = Player("Sean", size)
+
+    #Project 3 Scope video
+    print("Welcome to Battleships")
+    print(f"Board Size: {size} * {size}.")
+    print("Top left corner is row: 0, column: 0")
+    print("-" * 35)
 
     print("Player Board:")
     player_board.print()
