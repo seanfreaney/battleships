3ebcf6dd514d3ab402f2d5907b0fb1d4eb43beb2
seanfreaney <seanfreaney123@gmail.com>
2024-03-15 09:23:30 +0000

    create board class and game function

--- run.py
+++ run.py
@@ -1,2 +1,27 @@
-From random import randint
+import random
 
+
+
+class Board: 
+    """
+    Class to set board size and display.
+    """
+    def __init__(self, size):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)] #Project 3 logic/scope video
+    
+    def print(self):
+        for row in self.board:
+            print(" ".join(row))
+
+
+def game():
+
+    size = 5
+    board = Board(size)
+    board.print()
+
+game()
+
+   
+    
